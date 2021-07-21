16aab2a49b6a2389ab259b14bdc9be3565bac60b
Chanon Rattanajaratkul <77735847+NonRoute@users.noreply.github.com>
2021-07-21 10:50:24 +0700

    Add vehicles
    -Add vehicle objects
    -Add vehicle scripts

---
+++ Programming Theory Project/Assets/Scripts/vehicle.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vehicle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected void OnMouseDown() {
+        Debug.Log("This is vehicle");
+    }
+}
